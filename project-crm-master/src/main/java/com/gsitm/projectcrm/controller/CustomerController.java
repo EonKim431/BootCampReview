5f4c7428e1f031d5deeecbb120bcde733a35534a
DESKTOP-HV08OEA\earne <sunbea0730@gmail.com>
2026-06-10 16:10:34 +0900

    refactor: improve controller logging

--- project-crm-master/src/main/java/com/gsitm/projectcrm/controller/CustomerController.java
+++ project-crm-master/src/main/java/com/gsitm/projectcrm/controller/CustomerController.java
@@ -1,10 +1,10 @@
 package com.gsitm.projectcrm.controller;
 
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +18,14 @@ import com.gsitm.projectcrm.dto.CustomerDto;
 import com.gsitm.projectcrm.dto.TextLogDto;
 import com.gsitm.projectcrm.service.CustomerService;
 
-import lombok.extern.java.Log;
-import lombok.extern.slf4j.Slf4j;
-@Slf4j
 @Controller
 public class CustomerController {
+
+	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
+
 	@Autowired
-	CustomerService customerService;
-	
-	
+	private CustomerService customerService;
+
 	@GetMapping("/cust")
 	public String customView() {
 		return "customerView";
@@ -36,77 +35,81 @@ public class CustomerController {
 	public String customerSearch() {
 		return "customerSearch";
 	}
-	
+
 	@PostMapping("/TextLogAjax")
 	@ResponseBody
-	public List<TextLogDto> TextLog(@RequestBody Map<String, String> body) {
+	public List<TextLogDto> textLog(@RequestBody Map<String, String> body) {
 		String keywordStr = body.get("keyword");
-		System.out.println("keyword = " + keywordStr);
+		log.info("TextLogAjax request. keyword={}", keywordStr);
+
 		Long keyword = Long.parseLong(keywordStr);
-	
 		List<TextLogDto> textLogDtos = customerService.getDTOInfo(keyword);
-		System.out.println(textLogDtos.toString());
+
+		log.debug("TextLogAjax response. resultCount={}", textLogDtos.size());
 		return textLogDtos;
 	}
+
 	@PostMapping("/searchAjax")
 	@ResponseBody
 	public List<CustomerDto> getListAdmin(@RequestBody Map<String, String> body) {
 		String keyword = body.get("keyword");
-		System.out.println("keyword = " + keyword);
-//		List<AdminDto> adminDtos = adminService.searchAdminOne(keyword);
-		List<CustomerDto> dtos = new ArrayList<>();
-		dtos = customerService.listFind(keyword);
-		System.out.println(dtos.toString());
+		log.info("searchAjax request. keyword={}", keyword);
 
-		return dtos;
+		List<CustomerDto> customers = customerService.listFind(keyword);
+
+		log.debug("searchAjax response. resultCount={}", customers.size());
+		return customers;
 	}
+
 	@PostMapping("/searchAllAjax")
 	@ResponseBody
 	public List<CustomerDto> getListAllAdmin(@RequestBody Map<String, String> body) {
-//		List<AdminDto> adminDtos = adminService.searchAdminOne(keyword);
-		List<CustomerDto> dtos = new ArrayList<>();
-//		dtos = service.list();
-//		System.out.println(dtos.toString());
-//		body.put("a_id", "admin");
-		dtos = customerService.list();
-		
-		return dtos;
+		List<CustomerDto> customers = customerService.list();
+
+		log.debug("searchAllAjax response. resultCount={}", customers.size());
+		return customers;
 	}
 
 	@PostMapping("/searchOneAjax")
 	@ResponseBody
 	public CustomerDto getOneListAdmin(@RequestBody Map<String, String> body) {
 		String keywordStr = body.get("keyword");
-		Long keyword = Long.parseLong(keywordStr);
-//		System.out.println("keyword = " + keyword);
-	
-		CustomerDto dto = new CustomerDto(); 
-		dto = customerService.searchAdminOne(keyword); 
-		System.out.println(dto.toString());
-		 
-		return dto;
-	}
-	
-	@PostMapping("/registerCustomer")
-    @ResponseBody
-    public void registerCustomer(@ModelAttribute CustomerDto customerDTO) {
-		customerService.registerCustomer(customerDTO);
-    }
+		log.info("searchOneAjax request. keyword={}", keywordStr);
 
-    @PostMapping("/updateCustomer")
-    @ResponseBody
-    public void updateCustomer(@ModelAttribute CustomerDto customerDTO) {
-    	String a =  customerDTO.getCUST_NM();
-        System.out.println(customerDTO.getCUST_NM()+""+customerDTO.getEML_ADDR());
-        customerService.updateCustomer(customerDTO);
-    }
-    
-    @PostMapping("/deleteCustomer")
-    @ResponseBody
-    public String deleteCustomer(@RequestParam("CUST_SN") Long CUST_SN) {
-        customerService.deleteCustomer(CUST_SN);
-        return "Customer deleted successfully";
-    }
-	
+		Long keyword = Long.parseLong(keywordStr);
+		CustomerDto customer = customerService.searchAdminOne(keyword);
+
+		if (customer != null) {
+			log.debug("searchOneAjax response. custSn={}", customer.getCUST_SN());
+		} else {
+			log.debug("searchOneAjax response. customer not found. keyword={}", keyword);
+		}
+
+		return customer;
+	}
+
+	@PostMapping("/registerCustomer")
+	@ResponseBody
+	public void registerCustomer(@ModelAttribute CustomerDto customerDTO) {
+		customerService.registerCustomer(customerDTO);
+
+		log.info("registerCustomer completed.");
+	}
+
+	@PostMapping("/updateCustomer")
+	@ResponseBody
+	public void updateCustomer(@ModelAttribute CustomerDto customerDTO) {
+		customerService.updateCustomer(customerDTO);
+
+		log.info("updateCustomer completed. custSn={}", customerDTO.getCUST_SN());
+	}
+
+	@PostMapping("/deleteCustomer")
+	@ResponseBody
+	public String deleteCustomer(@RequestParam("CUST_SN") Long CUST_SN) {
+		customerService.deleteCustomer(CUST_SN);
+
+		log.info("deleteCustomer completed. custSn={}", CUST_SN);
+		return "Customer deleted successfully";
+	}
 }
- 
